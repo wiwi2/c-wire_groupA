a2ed158b22ba20aadd05ca29263e24d1c7ba0892
wiwi2 <72577403+wiwi2@users.noreply.github.com>
2024-11-21 17:54:03 +0100

    Update c-wire.sh

--- c-wire.sh
+++ c-wire.sh
@@ -1,4 +1,10 @@
-#!/bin/bash // à continuer.... pour l'instant juste pour verifier l'entrée du fichier en parametre
+#!/bin/bash 
+#à continuer....
+#pour l'instant juste pour verifier l'entrée du fichier en parametre
+
+
+#--> on va utiliser des arguments à envoyer vers le programme c pour les mettre en lien 
+
 
 # Vérifier si un argument (le chemin du fichier) a été fourni
 if [ $# -eq 0 ]; then
@@ -15,3 +21,9 @@ fi
 # Si tout est bon, afficher un message de confirmation
 echo "Le fichier '$1' existe et est prêt à être utilisé."
 
+
+
+./programme.c --> on appelle un fichier c pour qu'il soit exécuté puis on récupère sa sortie ?
+--> cat output.txt / par exemple, ça va je sais plus à voir...
+-> mais ca c'est si on ecrit avec le programme c... à faire pour la somme des conso ? je sais pas... 
+voir ca probleme de logique 
