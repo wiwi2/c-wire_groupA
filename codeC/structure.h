b3752a7238981741e3ef3c46c7e299d341ed2ad2
wiwi2 <72577403+wiwi2@users.noreply.github.com>
2024-11-21 16:37:54 +0100

    Update structure.h

--- codeC/structure.h
+++ codeC/structure.h
@@ -1,4 +1,18 @@
-typedef struct _AVL{
-    
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-} AVL*; 
+/* Structure AVL : stores the id of the station, its capacity (to transfer energy), the sum of 
+consommation of its consummers, plus its child... */ 
+typedef struct _AVL{
+    char* station_id;
+    int capacity;
+    int sum_conso; 
+    struct _AVL* pLeft; 
+    struct _AVL* pRight;
+    
+} AVL; 
+
+
+
+// penser à transformer tous les - par des 0 : prof's idea. 
