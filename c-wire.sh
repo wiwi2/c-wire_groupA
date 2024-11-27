fbf31b899991d9ddd117889ac8facbc36033c73e
wiwi2 <72577403+wiwi2@users.noreply.github.com>
2024-11-27 21:20:00 +0100

    Update c-wire.sh

--- c-wire.sh
+++ c-wire.sh
@@ -1,7 +1,7 @@
 
 NOTES PERSO A NE PAS SUPPRIMER POUR L INSTANT ! 
 
-les parametres quon doit mettre ; ==> enlever les - !
+les parametres quon doit mettre ;
 chemin du fichier (obligatoire)
 type de station (hvb , hva , lv ) (obligatoire)
 type de conso (comp , indiv , all ) (obligatoire) 
@@ -10,27 +10,19 @@ identifiant de centrale (attention spécial)
 option daide help 
 
 
-à faire : (voir ou mettre ça) pour le temps 
+à faire : (voir OU mettre ça) pour le temps 
 récup le temps dexecution en utilisant : 
 start_time = (date + %s) pour recup en secondes le temps 
 end_time = same
 puis : durée = end minus start 
 
-à faire aussi 
-ranger les exit dans lodre : ordre perdu à un moment 
-ranger avec fonctions et main après setre rensegné auprès du prof     HERE on s'en fout
 
 
-comment envoyer entre programme c et le shell aller retour...         HERE ok
-make ok oui
-puis pour recupérer ? pas trop capté ok normalement < > voir cut tout ca -> simple efficace
-
-pour le fichier csv : ou le mettre ?? il est trop gros pour github... HERE pas mettre
-et le chemin... fonctionne comme j ai fait ou ?? voir                 here normalement ok 
+comment envoyer entre programme c et le shell aller retour...  : cut .. < > 
+puis pour recupérer : ok normalement < > voir cut tout ca -> simple efficace
 
 
-
-normalement : 
+normalement :  OK
 main : commence par help, 
 puis verifie les arguments 
 avant de passer au traitement 
@@ -38,7 +30,6 @@ avant de passer au traitement
 quand cest clair >> passer au traitement du fichier 
 cut ...
 programme c...
-
 
 programme c : main idea 
 on rentre on scanf 
@@ -56,8 +47,9 @@ grep  a use pour les lv pour trier par ligne entre comp et indiv du coup
 tr pour changer - en 0 pour faciliter le calcul des conso 
 
 
-dans tests : juste un test comme le projet l'explique : un fichier texte quoi hva indiv ou autre 
+dans tests : juste un test comme le projet l explique : un fichier texte quoi hva indiv ou autre 
 on nomera le truc < hva_indiv.txt par exemple 
+
 on fera trois cas du coup ? hva , hvb et lv 
 avec des sous cas ..? pour all indiv etc et des cut.. à voir
 
@@ -75,23 +67,25 @@ avec des sous cas ..? pour all indiv etc et des cut.. à voir
 
 # Function to display the help
 help() {
-    echo "How to use: $0 <file_path> <type_station> <type_consumer> [centrale_id]"
+    echo -e "...................................HELP............................................\n"
+    echo -e "\n\nHow to use: $0 <file_path> <type_station> <type_consumer> [centrale_id]"
     echo
     echo "The various options :"
     echo "  file_path            : Path to the csv file (obligatory)" // ici voir comment lécrire
-    echo "  type_station         : Type of station to process. Possible values : -hvb, -hva, -lv (obligatory)"
-    echo "  type_consumer        : Type of consumer to process. Possible values : -comp, -indiv, -all (obligatory)"
-    echo "  centrale_id          : Id of the centrale (option)" // attention à ça 
+    echo "  type_station         : Type of station to process. Possible values : hvb, hva, lv (obligatory)"
+    echo "  type_consumer        : Type of consumer to process. Possible values : comp, indiv, all (obligatory)"
+    echo "  centrale_id          : Id of the centrale (option) : between 1 and 5 included." // attention à ça 
     echo "  -h                   : Display this help."
     echo -e "\n \nMore explanations :"
-    echo "-hvb ; -hva ; -lv : are for a station (HV-B station, HV-A station, LV post)" // ici anglais à voir
-    echo -e "\n-comp : to get companies' consumption of energy. \n-indiv : to get individuals' consumption of energy. \n-all : to get everyone's consumption of energy (individuals + companies).\n\n"
-    echo -e "Example : $0 <file_path> -hvb -comp 2\n\n"
-    echo "Warning :"
-    echo " The options that follow are prohibited : "
-    echo "    -hvb -all, -hvb -indiv, -hva -all, -hva -indiv"
+    echo "  hvb ; hva ; lv : are for a station (HV-B station, HV-A station, LV post)" // ici anglais à voir
+    echo -e "\n  comp : to get companies' consumption of energy under the station selected. \n  indiv : to get individuals' consumption of energy under the station selected. \n  all : to get everyone's consumption of energy (individuals + companies) under the station selected.\n\n"
+    echo -e "\nExample : $0 <file_path> -hvb -comp 2\n\n"
+    echo "Warnings :"
+    echo "1) The options that follow are prohibited : "
+    echo "     hvb all ; hvb indiv ; hva all ; hva indiv"
+    echo "2) Be sure that your file exists and that it is a csv one ! (with the extension csv)"
     echo
-    echo "If you use -h, regardless of other option(s), the help will be displayed"
+    echo "If you use -h, regardless of other option(s), the help will be displayed."
 }
 
 # if -h is encountered in the options, regardless of its position, the help will be displayed 
@@ -107,7 +101,7 @@ done
 if [ "$#" -lt 3 ]; # -lt = less than ; to check how many options there are
     then
     echo "Error : some options are missing."
-    echo "Reminder : ./prog /waytothefile.csv [typeofstation] [typeofconsumer]. Please try again." 
+    help
     exit 2
 fi
 
@@ -121,64 +115,100 @@ centrale_id="$4"
 if [ ! -f "$file_path" ] // pas sure pour le check 
     then 
     echo -e "Error : the file $file_path does not exist or the path is incorrect. \nPlease check its existence of the path to the file."
+    help
     exit 3
-elif [ "$file_path" == *.csv ]
+elif [ "$file_path" != *.csv ]
     then
     echo -e "Error : the file isn't a csv file. Please check and try again."
+    help
     exit 4
 fi
 
 
-# Cheching the options : type of station, type de consumer, id of a centrale... 
-# Checking the type of station (-hvb or -hva or -lv)
-if [[ "$type_station" != "-hvb" && "$type_station" != "-hva" && "$type_station" != "-lv" ]]; 
+# Cheching the options : type of station, type of consumer, id of a centrale... 
+# Checking the type of station (-hvb or -hva or -lv) first :
+if [ "$type_station" != "hvb" && "$type_station" != "hva" && "$type_station" != "lv" ]; 
 then
-    echo "Error : the type of station '$type_station' is invalid. Possible values : -hvb, -hva, -lv."
-    exit 2
-fi
-
-
-# Checking the type of consumer
-if [[ "$type_consommateur" != "-comp" && "$type_consommateur" != "-indiv" && "$type_consommateur" != "-all" ]]; 
-then
-    echo "Error : the type of consumer '$type_consommateur' is invalid. Possible values : -comp, -indiv, -all."
-    exit 3
-fi
-
-
-# Checking restrictions on certain parameter combinations
-# With hvb station
-if [[ "$type_station" == "-hvb" && ( "$type_consommateur" == "-all" || "$type_consommateur" == "-indiv" ) ]]; 
-then
-    echo "Error : the combinations '-hvb -all' and '-hvb -indiv' are prohibited."
-    exit 4
-fi
-
-# With hva station 
-if [[ "$type_station" == "-hva" && ( "$type_consommateur" == "-all" || "$type_consommateur" == "-indiv" ) ]]; 
-then
-    echo "Error : the combinations '-hva -all' and '-hva -indiv' are prohibited."
+    echo "Error : the type of station '$type_station' is invalid. Possible values : hvb, hva, lv."
+    help
     exit 5
 fi
 
-# Checking the existence of the option centrale_id ; as we know there are just 5 centrales, we check it therefore
-if [ ! -z "$centrale_id" ]; // si largument nest pas vide (car rappel : optionnel)
+
+# Checking the type of consumer :
+if [ "$type_consommateur" != "comp" && "$type_consommateur" != "indiv" && "$type_consommateur" != "all" ];  
 then
-    if ! [[ "$centrale_id" =~ ^[0-9]+$ ]]; 
-    then
-        echo "Error : The centrale's id must be a whole number."
-        exit 7
+    echo "Error : the type of consumer '$type_consommateur' is invalid. Possible values : comp, indiv, all."
+    help
+    exit 6
+fi
+
+
+# Checking the existence of the option centrale_id (because it's optionnal); as we know there are just 5 centrales, we check it therefore :
+if [ ! -z "$centrale_id" ]; # if the argument exist (argument != void) // voir ici 
+then
+    if [[ ! "$centrale_id" =~ ^[0-9]+$ ]]; then
+        echo "Error : The centrale's id must be a whole number between 1 and 5."
+        help
+        exit 9
+    fi
 # if the id is below 1 or above 5 : error
     elif [ "$centrale_id" -lt 1 ] || [ "$centrale_id" -gt 5 ]; 
         then
         echo "Error : The centrale's id must be between 1 and 5 (included)."
-        exit 8
+        help
+        exit 10
     fi
+fi
+
+# Checking restrictions on certain parameter combinations (like hvb all)
+# With hvb station :
+if [ "$type_station" == "hvb" && ( "$type_consommateur" == "all" || "$type_consommateur" == "indiv" ) ]; 
+then
+    echo "Error : the combinations 'hvb all' and 'hvb indiv' are prohibited."
+    help
+    exit 7
+fi
+
+# With hva station 
+if [ "$type_station" == "hva" && ( "$type_consommateur" == "all" || "$type_consommateur" == "indiv" ) ]; 
+then
+    echo "Error : the combinations 'hva all' and 'hva indiv' are prohibited."
+    help
+    exit 8
 fi
 
 
 
+# After checking everything related to the options, we start to get interested in the executable and the prog c... 
+# We store the of the expected executable (defined in our makefile) 
+EXECUTABLE="prog"
 
+# Check if the executable file exists
+if [ ! -f "$EXECUTABLE" ]; then # if not, we start the compilation...
+    # Run the compilation using make
+    if ! make; then # if the output of make isn't 0, the compilation failed
+        echo "Error: Compilation failed. Please try again."
+        exit 9
+    fi
+    
+    # Check if the executable was generated after compilation
+    if [ ! -f "$EXECUTABLE" ]; then
+        echo "Error: The executable '$EXECUTABLE' was not generated after compilation. Please try again."
+        exit 10
+    fi
+fi
+
+
+ici sert pas normalement mais voir : (
+# We execute the program with the arguments passed to the script
+./"$EXECUTABLE" "$@" // on rentre dans le programme c du coup ici ... ? voir quoi faire après )
+
+
+if hva indiv ... 
+cut ... ? faire un fichier avec les trucs intéréssants...
+puis envoyer vers le programme c pour qu il lise et prenne ce qui est intéressant directement 
+et fait donc la somme des conso (regarder photo... tout passe par le cut et les sorties > < )
 
 
 
