eb642ec689a98d4f0870b1c0b71e687467c171d0
wiwi2 <72577403+wiwi2@users.noreply.github.com>
2024-11-29 21:56:45 +0100

    Update main.c

--- codeC/main.c
+++ codeC/main.c
@@ -1,6 +1,26 @@
-// est ce qu'on va devoir ecrire avec le main et renvoyer des fichiers ouput ?
-// mieux comprendre ce qui va etre fait dans le main.. 
-#
-#
-#
+#include <stdio.h>
+#include <stdlib.h>
 #include "function.c"
+
+
+int main(){
+
+  // on aura des scanf etc...
+  // des printfs
+
+  // on va rentrer dans les AVL en recuperant d'abord avec des scanf les informations intéressantes 
+  // puis on va envoyer vers la creation d'AVL... insert AVL ... on traite toutes les informations
+  // puis de la on va sommer la consommation dans chaque noeud...
+
+  ATTENTION ; chaque noeud = hvb ou hva ou poste lv ! 
+on fait la somme dedans pour le hvb 1 par exemple... puis on retourne...
+
+
+
+
+
+
+
+
+
+
