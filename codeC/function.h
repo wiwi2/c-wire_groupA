f2b5217b384745b1807fb84fe5864b34e7b341a9
wiwi2 <72577403+wiwi2@users.noreply.github.com>
2024-12-02 10:47:48 +0100

    Update function.h

--- codeC/function.h
+++ codeC/function.h
@@ -1,1 +1,17 @@
 
+#include "structure.h"
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+
+// Function to create a new node
+Node* createNode(int id, int capacity, int total_consumption);
+    
+// AVL insert function (by ID)
+Node* insertAVL(Node *root, int id, int capacity, int total_consumption);
+
+// Postfix traversal to display and free nodes
+void displayAndFree(Node *root);
+
+#endif
+
