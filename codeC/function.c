6297594579e8f20109a8b9d11cde3ef84acb92ac
wiwi2 <72577403+wiwi2@users.noreply.github.com>
2024-12-08 17:15:22 +0100

    Update function.c

--- codeC/function.c
+++ codeC/function.c
@@ -220,21 +220,6 @@ void displayAndFree(Node *root) {
 }
 
 
-int main() {
-    Node *root = NULL;
-    int id, capacity, consumption;
-
-    // Read data using scanf and insert in the AVL
-    while (scanf("%d;%d;%d", &id, &capacity, &consumption) == 3) {
-        root = insertAVL(root, id, capacity, consumption); 
-    }
-      
-    // Display and free nodes in postfix order
-    displayAndFree(root);
-
-    return 0;
-}
-
 
 
  
