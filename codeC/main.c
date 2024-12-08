64f172725cd98953b318349fda688c2c2acafecd
wiwi2 <72577403+wiwi2@users.noreply.github.com>
2024-12-08 17:14:58 +0100

    Update main.c

--- codeC/main.c
+++ codeC/main.c
@@ -5,22 +5,6 @@
 
 int main(){
 
-  // on aura des scanf etc...
-  // des printfs
-
-  // on va rentrer dans les AVL en recuperant d'abord avec des scanf les informations intéressantes 
-  // puis on va envoyer vers la creation d'AVL... insert AVL ... on traite toutes les informations
-  // puis de la on va sommer la consommation dans chaque noeud...
-
-  ATTENTION ; chaque noeud = hvb ou hva ou poste lv ! 
-on fait la somme dedans pour le hvb 1 par exemple... puis on retourne...
-
-
-
-
-
-
-
-
-
+  
+}
 
