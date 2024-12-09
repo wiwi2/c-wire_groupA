fed1f49bd12185cdcf6ea5fd0dc546e5052b2816
snm-s <sanemhimana@hotmail.com>
2024-12-09 21:54:22 +0100

    Update function.c

--- codeC/function.c
+++ codeC/function.c
@@ -10,7 +10,7 @@
 AVL* createNodeAVL(int id, int capacity, int conso){
       AVL* New = malloc(sizeof(AVL)); 
       if(New==NULL){
-        printf("Failed to make some space for an AVL... \nPlease try again.\n"); 
+        printf("Failed to make some space for an AVL... \nPlease try again.\n"); //immediate stop if the allocation failed
         exit(1); 
       }
       New->station_id = id; 
@@ -80,51 +80,53 @@ AVL* LeftRotation(AVL* Tree){
       }
       AVL* pivot; // we create a pivot that will help us do a rotation  
 
-      pivot        = Tree->pRight; 
+      pivot        = Tree->pRight; //the right's son become the pivot
       Tree->pRight = pivot->pLeft; 
       pivot->pLeft = Tree; 
 
       int eq_a = Tree->eq; 
       int eq_p = pivot->eq; 
-  
+
+	//update of equilibrium factors
       Tree->eq  = eq_a - max2(eq_p,0) - 1; 
       pivot->eq = min3(eq_a - 2, eq_a + eq_p - 2, eq_a - 1); 
       Tree      = pivot; 
 
-      return Tree;
+      return Tree; //Tree become the new root
 }
 
-
+//function that do a simple right rotation in the tree to help us keep it equilibrated
 AVL* RightRotation(AVL* Tree){ // à faire comme celle d'avant 
       if(Tree == NULL){
             printf("Error : AVL is NULL. Please try again.");
             exit(2); 
       }
 
-      AVL* pivot; 
+      AVL* pivot; //we create a pivot that will help us do a rotation
 
       int eq_a; 
       int eq_p; 
 
-      pivot         = Tree->pLeft;
+      pivot         = Tree->pLeft; //the left's son become the pivot
       Tree->pLeft   = pivot->pRight; 
       pivot->pRight = Tree; 
 
       eq_a       = Tree->eq;
       eq_p       = pivot->eq; 
 
+	//update of equilibrium factors
       Tree->eq  = eq_a - min2(eq_p, 0) + 1; 
       pivot->eq = max3(eq_a + 2, eq_a + eq_p + 2, eq_p + 1); 
       Tree      = pivot; 
 
-      return Tree; 
+      return Tree; // Tree become the new root
 }
 
 
 // Double left rotation : to equilibrate the AVL Tree that we'll use to sum the consomations...
 AVL* DoubleLeftRotation(AVL* Tree){
       if(Tree == NULL){
-            printf("Error : AVL is NULL. Please try again."); 
+            printf("Error : AVL is NULL. Please try again."); //immediate stop if the allocation failed
             exit(60); 
 }
       Tree->pRight = RightRotation(Tree->pRight); 
@@ -132,10 +134,10 @@ AVL* DoubleLeftRotation(AVL* Tree){
 }
 
 
-// Double right rotation
+// Double right rotation : to equilibrate the AVL Tree that we'll use to sum the consomations...
 AVL* DoubleRightRotation(AVL* Tree){
       if(Tree == NULL){
-            printf("Error : AVL is NULL. Please try again."); 
+            printf("Error : AVL is NULL. Please try again."); //immediate stop if the allocation failed
             exit(30); 
 	}
       Tree->pLeft = LeftRotation(Tree->pLeft); 
@@ -151,19 +153,21 @@ AVL* equilibrageAVL(AVL* Tree){
             exit(50); 
       }
       if(Tree->eq >= 2){
+	      //case where the tree is unbalanced on the right
             if(Tree->pRight->eq >=0){
-                  return LeftRotation(Tree);
+                  return LeftRotation(Tree); // simple left rotation
             }
            else{
-                  return DoubleLeftRotation(Tree); 
+                  return DoubleLeftRotation(Tree); //double left rotation
                   }
 }
       else if(Tree->eq <= -2){
+	      //case where the tree is unbalanced on the left
             if(Tree->pLeft->eq <= 0){
-                  return RightRotation(Tree);
+                  return RightRotation(Tree); // simple right rotation
             }
             else{
-                  return DoubleRightRotation(Tree);
+                  return DoubleRightRotation(Tree); // double right rotation 
                   }
       }
       return Tree; 
@@ -174,37 +178,39 @@ AVL* equilibrageAVL(AVL* Tree){
 AVL* insertAVL(AVL* tree, int* h, int id, int capacity, int conso) { // fuite mémoire ici , voir capa et conso mise à jour au meme endroit...
 
     if (tree == NULL) {
-    AVL* New = createNodeAVL(id, capacity, conso);
+    AVL* New = createNodeAVL(id, capacity, conso); // if the tree is empty, create a new node
     if (New == NULL) {
         printf("The malloc of the new AVL node failed. Please try again.");
         exit(1);
     }
-        *h = 1;
+        *h = 1; //the height has increased
         return New;
     }
 
  
 	if (id < tree->station_id) {
+		// if the element id is smaller, insert on the left
   	  tree->pLeft = insertAVL(tree->pLeft, h, id, capacity, conso);
     	*h = -(*h);
 	} else if (id > tree->station_id) {
+		//if the element is is bigger, insert on the right
    	 tree->pRight = insertAVL(tree->pRight, h, id, capacity, conso);
-	} else {
+	} else { //element id's already present
   	  tree->sum_conso += conso;
   	  *h = 0;
   	  return tree;
 	}
 
 
-    // Balance factor update here
+    // update of equilibrium factors 
     if (*h != 0) {
         tree->eq = tree->eq + *h;
         tree = equilibrageAVL(tree);
 
-        if (tree->eq == 0) {
+        if (tree->eq == 0) { //height's update
             *h = 0;
         } else {
-            *h = 1;
+            *h = 1; 
         }
     }
 
