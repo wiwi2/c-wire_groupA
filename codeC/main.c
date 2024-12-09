cc2631eacfff6e8778db015c995b75e56c1edd57
wiwi2 <72577403+wiwi2@users.noreply.github.com>
2024-12-09 16:37:16 +0100

    Update main.c

--- codeC/main.c
+++ codeC/main.c
@@ -1,13 +1,19 @@
-#include "function.c"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "function.h"
 
 
 int main() {
     AVL* tree = NULL;
     int id, capacity, consumption;
+    
+    int h = 0;
 
     // Read data using scanf and insert in the AVL
     while (scanf("%d;%d;%d", &id, &capacity, &consumption) == 3) {
-         tree = insertAVL(tree, id, capacity, consumption); 
+         tree = insertAVL(tree, &h, id, capacity, consumption); 
     }
       
     // Display and free nodes in postfix order
