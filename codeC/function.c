73db518b6398df19f23171f8b9f289c3d755e20e
sagana-21 <133916169+sagana-21@users.noreply.github.com>
2024-12-09 11:16:52 +0100

    Update function.c

--- codeC/function.c
+++ codeC/function.c
@@ -3,8 +3,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "structure.h"
-
+ 
 AVL* createNodeAVL(int id, int capacity, int conso){
+      // AVL declaration 
       AVL* New = malloc(sizeof(AVL)); 
       if(New==NULL){
         printf("Failed to make some space for an AVL... \nPlease try again.\n"); 
@@ -83,22 +84,21 @@ AVL* LeftRotation(AVL* Tree){
       Tree->pRight = pivot->pLeft; 
       pivot->pLeft = Tree; 
   
-      Tree->eq  = eq_a - max2(eq_p,0) - 1; 
-      pivot->eq = min3(eq_a - 2, eq_a + eq_p - 2, eq_a - 1); 
+      Tree->eq  = eq_a - max2(eq_p,0) - 1; // adjusts Tree->eq based on the value of eq_p, if eq_p is positive or zero, the value will be reduced further
+      pivot->eq = min3(eq_a - 2, eq_a + eq_p - 2, eq_a - 1); // assigns pivot->eq to the smallest value among three options, based on eq_a and eq_p
       Tree      = pivot; 
 
       return Tree;
 }
 
-
-AVL* RightRotation(AVL* Tree){ // à faire comme celle d'avant 
+// function that do a simple right rotation in the tree to help us keep it equilibrated 
+AVL* RightRotation(AVL* Tree){ 
       if(Tree == NULL){
             printf("Error : AVL is NULL. Please try again.");
-            exit(2); 
+            exit(3); 
       }
 
-      AVL* pivot; 
-
+      AVL* pivot; // we create a pivot that will help us do a rotation
       int eq_a = Tree->eq;
       int eq_p = pivot->eq;
 
@@ -106,30 +106,32 @@ AVL* RightRotation(AVL* Tree){ // à faire comme celle d'avant
       Tree->pleft   = pivot->pRight; 
       pivot->pRight = Tree; 
 
-      Tree->eq  = eq_a - min2(eq_p, 0) + 1; 
-      pivot->eq = max3(eq_a + 2, eq_a + eq_p + 2, eq_p + 1); 
+      Tree->eq  = eq_a - min2(eq_p, 0) + 1; // adjusts Tree->eq based on the comparison between eq_p and 0, then adding 1
+      pivot->eq = max3(eq_a + 2, eq_a + eq_p + 2, eq_p + 1); // assigns to pivot->eq the largest value among three expressions involving eq_a and eq_p
+
+
       Tree      = pivot; 
 
       return Tree; 
 }
 
 
-// Double left rotation : to equilibrate the AVL Tree that we'll use to sum the consomations...
+// Double left rotation : to balance the AVL Tree that we'll use to sum the consomations...
 AVL* DoubleLeftRotation(AVL* Tree){
       if(Tree == NULL)
             printf("Error : AVL is NULL. Please try again."); 
-            exit(..); 
+            exit(4); 
 
       Tree->pRight = RightRotation(Tree->pRight); 
       return LeftRotation(Tree); 
 }
 
 
-// Double right rotation
+// Double right rotation : to balance the AVL Tree that we'll use to sum the consomations...
 AVL* DoubleRightRotation(AVL* Tree){
       if(Tree == NULL) 
             printf("Error : AVL is NULL. Please try again."); 
-            exit(..); 
+            exit(5); 
 
       Tree->pLeft = LeftRotation((Tree->pLeft); 
       return RightRotation(Tree); 
@@ -138,12 +140,12 @@ AVL* DoubleRightRotation(AVL* Tree){
 
 
 // Function to know what rotation is suitable to the situation of the AVL Tree 
-AVL* equilibrageAVL(AVL* Tree){
+AVL* balancingAVL(AVL* Tree){
       if(Tree==NULL){
             printf("Error : AVL is NULL. Please try again."); 
-            exit(..); 
+            exit(6); 
       }
-      if(Tree->eq >= 2){
+      if(Tree->eq >= 2){ // under right tree deeper
             if(Tree->pRight->eq >=0){
                   return LeftRotation(Tree)
             }
@@ -151,7 +153,7 @@ AVL* equilibrageAVL(AVL* Tree){
                   return DoubleLeftRotation(Tree); 
                   }
 }
-      else if(Tree->eq <= -2){
+      else if(Tree->eq <= -2){ // under left tree deeper
             if(Tree->pLeft->eq <= 0){
                   return RightRotation(Tree)
             }
@@ -168,17 +170,17 @@ AVL* insertAVL(AVL* Tree, int* h, int id, int capacity, int conso) {
     AVL* New = createNodeAVL(id, capacity, conso);
     if (New == NULL) {
         printf("The malloc of the new AVL node failed. Please try again.");
-        exit(1);
+        exit(7);
     }
 
     if (Tree == NULL) {
-        *h = 1;
+        *h = 1; // we insert an element : the balance will change
         return New;
     }
 
     if (id < Tree->id) {
         Tree->pLeft = insertAVL(Tree->pLeft, h, id, capacity, conso);
-        *h = -(*h);
+        *h = -(*h); // difference of -1 if we insert on the left
     } else if (id > Tree->id) {
         Tree->pRight = insertAVL(Tree->pRight, h, id, capacity, conso);
     } else {
@@ -189,12 +191,12 @@ AVL* insertAVL(AVL* Tree, int* h, int id, int capacity, int conso) {
     }
 
     // Balance factor update here
-    if (*h != 0) {
-        Tree->eq = Tree->eq + *h;
-        Tree = equilibrageAVL(Tree);
+    if (*h != 0) { // if there is a change
+        Tree->eq = Tree->eq + *h; // ...update of balance factors
+        Tree = balancingAVL(Tree);
 
-        if (Tree->eq == 0) {
-            *h = 0;
+        if (Tree->eq == 0) { // if tree is balance again...
+            *h = 0; // his ancestors doo not changes
         } else {
             *h = 1;
         }
