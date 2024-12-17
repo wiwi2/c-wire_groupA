0ac2acb241b8214b239b117f44a56efe30788aca
wiwi2 <72577403+wiwi2@users.noreply.github.com>
2024-12-17 17:24:28 +0100

    Update structure.h

--- codeC/structure.h
+++ codeC/structure.h
@@ -2,7 +2,7 @@
 #define STRUCTURES_H
 
 /* Structure AVL : stores the id of the station, its capacity (to transfer energy), the sum of 
-consommation of its consummers, plus its child and the value to keep the AVL equilibrated... */ 
+consommation of its consummers, plus its child and the value to keep the AVL balanced... */ 
 typedef struct _AVL{
     int station_id;
     long int capacity;
